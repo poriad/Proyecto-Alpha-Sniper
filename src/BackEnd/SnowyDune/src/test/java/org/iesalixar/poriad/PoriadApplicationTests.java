edd0a3edfc6500c7d9a6c266295a8de43255a2f6
Pablo Oria de Rueda <57188708+poriad@users.noreply.github.com>
2020-10-12 20:25:30 +0200

    Add files via upload

---
+++ src/BackEnd/SnowyDune/src/test/java/org/iesalixar/poriad/PoriadApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iesalixar.poriad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoriadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
